a0f4660a5965494619d61b7e673ae96dd68922a1
Anthony Bishopric <git@anthonybishopric.com>
2015-08-31 22:33:57 -0700

    switch to pixi

--- polaris.js
+++ polaris.js
@@ -1,7 +1,58 @@
+function Stellar(id, name, techLevel, x, y) {
+	this.id = id;
+	this.name = name;
+	this.population = 5000;
+	this.shipProduction = 20;
+	this.techLevel = 1;
+	this.x = x;
+	this.y = y;
+	this.ratios = {
+		tech: 0.3,
+		pop: 0.4,
+		build: 0.3
+	};
+	this.executeTurn = function() {
+		// every turn, your population rises by (pop * ratios.pop / 100)
 
-function onFrame(event) {
-	d3.select("#container").selectAll("p")
-	.data([Math.random(), Math.random()])
-	.enter().append("p")
-	.text(function(d){ return d; });
+	};
 }
+
+function JumpMap() {
+	this.map = {}
+	this.getConnections = function(stellar) {
+		var systems = this.map[stellar.id];
+		var res = [];
+		if (systems != null) {
+			for (var system in systems) {
+				if (systems.hasOwnProperty(system)) {
+					res.push(systems.get(system));
+				}
+			}
+		}
+		return res;
+	}
+	this.addConnection = function(left, right) {
+		var leftConns = this.map[left.id];
+		if (leftConns == null) {
+			leftConns = {};
+			this.map[left.id] = leftConns;
+		}
+		var rightConns = this.map[right.id];
+		if (rightConns == null) {
+			rightConns = {};
+			this.map[right.id] = rightConns;
+		}
+		leftConns[right.id] = new JumpConnection(right, left);
+		rightConns[left.id] = new JumpConnection(left, right);
+		return [leftConns[right.id], rightConns[left.id]];
+	}
+}
+
+function JumpConnection(stellarA, stellarB) {
+	this.stellarA = stellarA;
+	this.stellarB = stellarB;
+	this.active = false;
+	this.activate = function() { this.active = true; };
+	this.deactivate = function() { this.active = false; };
+}
+
